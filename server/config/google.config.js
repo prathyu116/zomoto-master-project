ad188ffd71cdc938e5a9a2b903b7b5713e529c5c
prathyu116 <prathyuprasad04669@gmail.com>
2021-10-11 13:36:17 +0530

    server part complte

--- server/config/google.config.js
+++ server/config/google.config.js
@@ -1,49 +1,51 @@
-// import googleOAuth from "passport-google-oauth20";
-// import passport from "passport";
+import googleOAuth from "passport-google-oauth20";
+import passport from "passport";
 
-// import { Usermodel } from "../database/allModel";
+import { Usermodel } from "../database/allModel";
 
-// const GoogleStratergy = googleOAuth.Strategy;
+const GoogleStratergy = googleOAuth.Strategy;
 
-// export default (passport) => {
-//     passport.use(
-//         new GoogleStratergy({
-//             clientID:process.env.GOOGLE_CLIENT_ID,
-//             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-//             callbackURL:"http://localhost:4001/auth/google/callback"
-//         },
-//         async(accessToken,refreshToken,profile,done)=>{
-//             //creating new user
-//             const newUser = {
-//                 fullname:profile.displayName,
-//                 email: profile.emails[0].value,
-//                 profilePic: profile.photos[0].value
-//             };
-//             try{
-//                   //check whether user exists or not
-//                   const user = await Usermodel.findOne({email: newUser.email});
-                
-//                   if(user){
-//                         //generate jwt token
-//                   const token = user.generateJwtToken();
-//                     //   return user
-//                       done(null,{user,token});
-//                   }else {
-//                       //creating new user
-//                       const user = await Usermodel.create(newUser)
-//                       //return user
-//                       done(null,{user,token});
-//                   }
+export default (passport) => {
+    passport.use(
+        new GoogleStratergy({
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:4001/auth/google/callback"
+        },
+            async (accessToken, refreshToken, profile, done) => {
+                //creating new user
+                const newUser = {
+                    fullname: profile.displayName,
+                    email: profile.emails[0].value,
+                    profilePic: profile.photos[0].value
+                };
+                try {
+                    //check whether user exists or not
+                    const user = await Usermodel.findOne({ email: newUser.email });
+
+                    if (user) {
+                        //generate jwt token
+                        const token = user.generateJwtToken();
+                        //   return user
+                        done(null, { user, token });
+                    } else {
+                        //creating new user
+                        const user = await Usermodel.create(newUser)
+                        //generating jwt token
+                        const token = user.generateJwtToken();
+                        //return user
+                        done(null, { user, token });
+                    }
 
 
-//             }catch(error){
-//                 done(error,null);
-//             }
-//         }
-//         )
+                } catch (error) {
+                    done(error, null);
+                }
+            }
+        )
 
-//     );
-//     passport.serializeUser((userData,done) => done(null, {...userData})); //these are callback
-//     passport.deserializeUser((id, done) => done(null, id));
-    
-// }
+    );
+    passport.serializeUser((userData, done) => done(null, { ...userData })); //these are callback
+    passport.deserializeUser((id, done) => done(null, id));
+
+}
--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-// import passport from "passport";
+import passport from "passport";
 
-//congig google auth
-// import googleAuthConfig from './config/google.config';
+// congig google auth
+import googleAuthConfig from './config/google.config';
+
 import routeConfig from "./config/route.config";
 
 //api
@@ -20,7 +21,6 @@ import  Order  from "./API/Orders";
 import  Review  from "./API/Review";
 //Database Connection
 import ConnectDB from './database/connection';
-import passport from "passport";
 
 const zomato = express();
 
@@ -28,11 +28,11 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({extended: false}));
 zomato.use(helmet());
 zomato.use(cors());
-// zomato.use(passport.initialize());
-// zomato.use(passport.session());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
 
-//passport configuration
-// googleAuthConfig(passport)
+// passport configuration
+googleAuthConfig(passport)
 routeConfig(passport);
 
 //For Application Routes localhost:4000/auth/signup
